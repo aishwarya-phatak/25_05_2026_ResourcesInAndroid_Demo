d9e4060c71bad2c679784e932bfb3ab00d15d754
aishwarya <ashp17star@gmail.com>
2026-06-04 13:46:48 +0530

    Resources In Android

--- app/src/main/java/com/example/a25_05_2026_resourcesinandroid_demo/MainActivity.java
+++ app/src/main/java/com/example/a25_05_2026_resourcesinandroid_demo/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.a25_05_2026_resourcesinandroid_demo;
 
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +14,49 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Resources resources;
+    int colourGreen;
+    int valueOfOne;
+    int [] numbers;
+    String welcomeAndroid;
+    String [] students;
+    float width;
+    TypedArray colourNames;
+    ImageView imageView1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
+        resources = getResources();
+        colourGreen = resources.getColor(R.color.green,null);
+        valueOfOne = resources.getInteger(R.integer.one);
+        numbers = resources.getIntArray(R.array.numbers);
+        welcomeAndroid = resources.getString(R.string.welcome_android);
+        students = resources.getStringArray(R.array.students);
+        width = resources.getDimension(R.dimen.width);
+        colourNames = resources.obtainTypedArray(R.array.colour_names);
 
+        imageView1.setImageResource(R.drawable.image_1);
+        Log.e("tag","" + colourGreen);
+        Log.e("tag",valueOfOne + "");
+        Log.e("tag" ,welcomeAndroid);
+
+        for(int i = 0; i < numbers.length; i++) {
+            Log.e("tag",numbers[i] + "");
+        }
+
+        Log.e("tag","===========================");
+
+//      for each loop
+        for (int eachNumber: numbers) {
+            Log.e("tag",eachNumber + "");
+        }
+
+        Log.e("tag","===========================");
+        for (String eachStudent : students) {
+            Log.e("tag",eachStudent);
+        }
     }
 }
